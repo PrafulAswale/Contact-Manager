83f3a8325612ad3cfbbc6c6ca4f397edf9f7703d
Praful Aswale <prafulcode15@gmail.com>
2023-07-19 17:20:45 +0530

    all pakages installed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,36 @@
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { ToastContainer } from "react-toastify";
+
 import "./App.css";
 
 function App() {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    const data = [];
+    const promise = async () => {
+      await fetch("https://jsonplaceholder.typicode.com/users/")
+        .then((response) => response.json())
+        .then((json) => {
+          json.map((contact) => {
+            return data.push({
+              id: contact.id,
+              name: contact.name,
+              number: contact.phone,
+              email: contact.email,
+            });
+          });
+        });
+      dispatch({ type: "FETCH_CONTACTS", payload: data });
+    };
+    promise();
+  });
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <ToastContainer />
+      <header className="App-header">
+        <h1>Home</h1>
+      </header>
     </div>
   );
 }
